a80e639969ad976961fabc17676169b8b1e67700
Thanh <quang.thanh.ta@pr1meti.de>
2023-12-17 17:20:07 +0100

    fix: Wrong sobel filter direction

--- src/convolution/Filter.cpp
+++ src/convolution/Filter.cpp
@@ -16,16 +16,16 @@ namespace sobel {
 
 image::Matrix<double> create_horizontal_filter_kernel() {
   return image::Matrix<double>{
-      {1.0, 0.0, -1.0},
-      {2.0, 0.0, -2.0},
-      {1.0, 0.0, -1.0},
+      {1.0, 2.0, 1.0},
+      {0.0, 0.0, 0.0},
+      {-1.0, -2.0, -1.0},
   };
 }
 image::Matrix<double> create_vertical_filter_kernel() {
   return image::Matrix<double>{
-      {1.0, 2.0, 1.0},
-      {0.0, 0.0, 0.0},
-      {-1.0, -2.0, -1.0},
+      {1.0, 0.0, -1.0},
+      {2.0, 0.0, -2.0},
+      {1.0, 0.0, -1.0},
   };
 }
 image::Matrix<double> create_diagonal_filter_kernel() {
